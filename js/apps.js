6a538cd0fc621b647891a8b1fdb3b8a75d90a6cd
Geraldine Firdaus <48635410+bantutataweb@users.noreply.github.com>
2023-05-15 16:26:14 +0700

    apps.js

--- js/apps.js
+++ js/apps.js
@@ -3,15 +3,15 @@ $(document).ready(()=>{
         var kata = $("#input_kata").val();
             halaman = document.getElementById("search")
             // console.log(halaman) 
-            $.getJSON("https://kbbi-api-amm.herokuapp.com/search?q="+kata, (json) => {
-                // console.log(json)
-                var judul = json["data"]["resultLists"][0]["kata"];
-                    arti  = json["data"]["resultLists"][0]["arti"];
-                     i = 0
-                    while (i < arti.length) {
-                        halaman.innerHTML += ('<div class="card p-1 shadow mt-2 mb-3"><div class="card-body"><b class="text-danger">'+judul+'</b><p>'+arti[i]+'</p></div></div>')
-                        i++
-                    }
+            $.getJSON("https://kbbi-api-zhirrr.vercel.app/api/kbbi?text="+kata, (json) => {
+                console.log(json)
+//                 var judul = json["data"]["resultLists"][0]["kata"];
+//                     arti  = json["data"]["resultLists"][0]["arti"];
+//                      i = 0
+//                     while (i < arti.length) {
+//                         halaman.innerHTML += ('<div class="card p-1 shadow mt-2 mb-3"><div class="card-body"><b class="text-danger">'+judul+'</b><p>'+arti[i]+'</p></div></div>')
+//                         i++
+//                     }
             })
     })
 
